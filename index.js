2889b11f970be1e3586e5bfc5b0e07b57d4bee01
migtarx <migtarx@gmail.com>
2021-11-25 19:40:27 +0100

    Prev changes

--- index.js
+++ index.js
@@ -9,30 +9,43 @@ var interval = parser.parseExpression(cron_patern);
 
 console.log(`Minecraft Auto Overviewer Ready!`.green + ` Next Render scheduled for ${interval.next().toString()}`.yellow);
 var job = new CronJob(cron_patern, function() {
+    for (var server of config.servers){
+        console.log(`${server.name} render Started!`.green);
 
-    console.log('New Render Schedule Started!'.green);
-    var render_shell_out = shell.exec(`python${config.python_ver} ${config.minecraft_overviewer_loc} --config=${config.minecraft_overviewer_configfile_loc}`);
-    var renderResult = (render_shell_out.substring(render_shell_out.length - 28, render_shell_out.length)).replace(/\s+/g, '');
-
-    if (renderResult != "openindex.htmltoviewit.") {
-        console.log(`Error Minecraft Overviewer Render Failed`.red);
-        ErrorExit();
-    } else {
-        console.log('Render complete!'.green);
-    };
-
-    console.log('Copying new assets'.yellow);
-    for (var i = 0; i < config.assets.length; i++) {
+        console.log(`Extracting ${server.name}'s world from docker container...`.yellow)
         try {
-            shell.cp(`./assets/${config.assets[i]}`, `${config.render_out_dir}`);
+            shell.exec(`docker cp ${server.container_id}:/home/container/qb_multi/slot1 /home/wrenders/sources/${server.name}`);
         } catch (error) {
-            console.log('Error while copying assets to render folder'.red);
-            ErrorExit()
+            console.log('Error while extracting source world from docker container'.red);
+            ErrorExit();
         }
-        console.log(` => ${config.assets[i]} copied to render folder.`.cyan);
+        console.log(`Extraction completed`.green);
+
+        var render_shell_out = shell.exec(`python${config.python_ver} ${config.minecraft_overviewer_loc} --config=${config.minecraft_overviewer_configfile_loc}`);
+        var renderResult = (render_shell_out.substring(render_shell_out.length - 28, render_shell_out.length)).replace(/\s+/g, '');
+
+        if (renderResult != "openindex.htmltoviewit.") {
+            console.log(`Error Minecraft Overviewer Render Failed`.red);
+            ErrorExit();
+        } else {
+            console.log('Render complete!'.green);
+        }
+
+        console.log('Copying new assets'.yellow);
+        for (var i = 0; i < config.assets.length; i++) {
+            try {
+                shell.cp(`./assets/${config.assets[i]}`, `${config.render_out_dir}`);
+            } catch (error) {
+                console.log('Error while copying assets to render folder'.red);
+                ErrorExit();
+            }
+            console.log(` => ${config.assets[i]} copied to render folder.`.cyan);
+        }
+        console.log('New Assets Copied'.green);
+        console.log(`AUTO RENDER COMPLETE!`.green + ` Last Render: ${interval.prev().toString()}`.cyan);
     }
-    console.log('New Assets Copied'.green);
-    console.log(`AUTO RENDER COMPLETE!`.green + ` Last Render: ${interval.prev().toString()}`.cyan);
+    
+    
     console.log(`Next Render scheduled for ${interval.next().toString()}`.yellow);
 }, null, true, config.time_zone);
 job.start();
