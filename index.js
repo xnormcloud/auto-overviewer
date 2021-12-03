8f6c2c210578dc933006b8d8c273b475565978ce
migtarx <migtarx@gmail.com>
2021-12-03 20:59:47 +0100

    Added email base code
    Added error and success bulk rendering messages to admins | Added current severs contact template emails

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const CronJob = require('cron').CronJob;
 const shell = require('shelljs');
 const colors = require('colors');
 const parser = require('cron-parser');
+const nodemailer = require("nodemailer");
 const config = require('./config.json');
+const mailer_config = require('./mailer_config.json');
 
 const cron_patern = `${config.min} ${config.hour} ${config.dayom} ${config.month} ${config.dayow}`;
 var interval = parser.parseExpression(cron_patern);
@@ -21,8 +23,6 @@ var job = new CronJob(cron_patern, function() {
         }
         console.log(`Extraction completed`.green);
         console.log(`Starting rendering process`.cyan)
-
-
 
         var render_shell_out = shell.exec(`python${config.python_ver} ${config.minecraft_overviewer_loc} --config=${config.minecraft_overviewer_configfile_loc}`);
         var renderResult = (render_shell_out.substring(render_shell_out.length - 28, render_shell_out.length)).replace(/\s+/g, '');
@@ -48,14 +48,70 @@ var job = new CronJob(cron_patern, function() {
         console.log('New Assets Copied'.green);
         console.log(`${server.name} AUTO RENDER COMPLETE!`.green + ` Last Render: ${interval.prev().toString()}`.cyan);
     }
-    
+    BulkComplete()
     console.log("Bulk worlds renders complete!".green)
     console.log(`Next Bulk Render scheduled for ${interval.next().toString()}`.yellow);
 }, null, true, config.time_zone);
 job.start();
 
+
+function BulkComplete(){
+    const transporter = nodemailer.createTransport({
+        host: mailer_config.contacter_server,
+        port: 465,
+        secure: true,
+        auth: {
+            user: config.contacter,
+            pass: config.contacter_pass
+        }
+    })
+    
+    const mailOptions = {
+        from: `Xnorm World <${config.contacter}>`,
+        to: 'xnorm.online@gmail.com',
+        subject: `Bulk Rendering Complete!`,
+        text: "All scheduled renders were completed",
+    }
+    
+    transporter.sendMail(mailOptions, (error, info)=>{
+        if(error){
+            console.log("Crash email report failed".red);
+            res.send('error');
+        } else {
+            console.log(`Email sent to Xnorm`.green);
+            res.send('success')
+        }
+    })
+}
 function ErrorExit() {
     console.log(`Failed to complete Minecraft Auto Overviewer schedule`.red)
+    console.log(`Sending cash notification to Xnorm Admins`.cyan)
+    const transporter = nodemailer.createTransport({
+        host: mailer_config.contacter_server,
+        port: 465,
+        secure: true,
+        auth: {
+            user: config.contacter,
+            pass: config.contacter_pass
+        }
+    })
+    
+    const mailOptions = {
+        from: `Xnorm World <${config.contacter}>`,
+        to: 'xnorm.online@gmail.com',
+        subject: `System Fail`,
+        text: "Something went wrong, and Minecraft Auto Overviewer stopped",
+    }
+    
+    transporter.sendMail(mailOptions, (error, info)=>{
+        if(error){
+            console.log("Crash email report failed".red);
+            res.send('error');
+        } else {
+            console.log(`Email sent to Xnorm`.green);
+            res.send('success')
+        }
+    })
     console.log(`Exiting Minecraft Auto Overviewer...`.yellow)
     process.exit();
 }
